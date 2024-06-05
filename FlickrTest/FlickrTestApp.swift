295cca302448f6038ed553a235e664604ce320d1
Gabriel Todaro <gabriel.todaro@outlook.com>
2024-06-05 14:19:39 -0300

    Initial Commit

---
+++ FlickrTest/FlickrTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrTestApp.swift
+//  FlickrTest
+//
+//  Created by Gabriel Patané Todaro on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
